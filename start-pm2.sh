3c0d3b779085d6810d1f60e224fddb9d82eb1b5b
Devadatta Sahoo <webmaster@nettantra.com>
2026-07-08 16:16:00 +0530

    Remove the PM2 deployment layer
    The binary self-daemonizes, so start-pm2.sh and ecosystem.config.js
    are gone; npm run deploy now rebuilds the bundle and restarts via
    hycanvas service restart. Docs and comments updated to match.
    
    Authored-By-Org: NetTantra Technologies

--- start-pm2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CWD=`CPWD=$(pwd);cd $(dirname $0);pwd;cd $CPWD`
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-cd $CWD
-nvm use
-pm2 start ecosystem.config.js
